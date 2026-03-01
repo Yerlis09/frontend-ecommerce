6ac3e66e84587c39f324cad78010042c44f912c4
yerlysmaria.castellarjulio <yerlysmaria.castellarjulio@amaris.com>
2026-03-01 01:42:11 -0500

    feat: add CartPage component and styles

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from '../pages/HomePage';
 import { ProductDetailPage } from '../pages/ProductDetailPage';
+import { CartPage } from '../pages/CartPage';
 
 export const AppRouter = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/product/:id" element={<ProductDetailPage />} />
+      <Route path="/cart" element={<CartPage />} />
     </Routes>
   </BrowserRouter>
 );
